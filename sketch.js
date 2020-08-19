34fa408ac7e49b58052f51388933478b568c1397
IDontKnow54321 <68257619+IDontKnow54321@users.noreply.github.com>
2020-08-19 16:51:37 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+//Namespacing
+const Engine=Matter.Engine;
+const World= Matter.World;
+const Bodies= Matter.Bodies;
+
+var engine,world,ground,ball;
+
+function setup() {
+  createCanvas(400,400);
+  engine=Engine.create();
+  world= engine.world;
+  var options={
+    isStatic:true
+  }
+  ground= Bodies.rectangle(200,390,400,20,options);
+  World.add(world,ground);
+ 
+  var ball_options={
+restitution:0.8
+  }
+  
+  ball= Bodies.circle(200,20,20,ball_options);
+  World.add(world,ball);
+}
+
+function draw() {
+  background(0);  
+  Engine.run(engine); //Engine.update(engine)
+  rectMode(CENTER);
+  rect(ground.position.x,ground.position.y,400,20)
+  ellipseMode(RADIUS);
+  ellipse(ball.position.x,ball.position.y,20,20);
+  drawSprites();
+}
